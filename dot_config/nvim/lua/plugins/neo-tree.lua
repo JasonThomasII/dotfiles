bf0a138862211947011bae6953c73871d120c3e1
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-04-18 11:26:59 -0500

    add nvim

---
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      position = "right",
+    },
+    filesystem = {
+      filtered_items = {
+        show_hidden_count = true,
+        hide_dotfiles = false,
+      },
+    },
+  },
+}
