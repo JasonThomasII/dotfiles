1c0bcb293688cd99adbfee2d1b2934d1a5d9887c
Andres Rodriguez <16929001+AndresRodH@users.noreply.github.com>
2024-05-21 12:15:45 -0500

    Update .config/nvim/lua/plugins/oil.lua

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -8,7 +8,7 @@ return {
 		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		keys = {
-			{ "<leader>e", "<cmd>Oil<CR>", desc = "Open file [E]xplorer" },
+			{ "<leader>e", "<cmd>Oil --float<CR>", desc = "Open file [E]xplorer" },
 		},
 		opts = {
 			default_file_explorer = true,
